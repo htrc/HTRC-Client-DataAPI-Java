608491c0f535c908a7ed5469a2951c880da93e4a
Boris Capitanu <capitanu@illinois.edu>
2014-01-19 00:39:33 +0000

    [maven-release-plugin]  copy for tag 0.6.3

---
+++ HTRC-Client-DataAPI-Java/tags/0.6.3/trunk/src/main/java/edu/indiana/d2i/htrc/clients/dataapi/HTRCConstants.java
@@ -0,0 +1,42 @@
+/*
+#
+# Copyright 2012 The Trustees of Indiana University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -----------------------------------------------------------------
+#
+# Project: knn
+# File:  HTRCConstaints.java
+# Description:  
+#
+# -----------------------------------------------------------------
+# 
+ */
+
+package edu.indiana.d2i.htrc.clients.dataapi;
+
+public class HTRCConstants {
+	// data api
+	public static String HOSTS_SEPARATEDBY_COMMA = "htrc.api.hosts";
+	public static String DATA_API_EPR = "htrc.api.epr";
+	public static String DATA_API_URL_DELIMITOR = "htrc.api.delimitor";
+	public static String DATA_API_CLIENTID = "htrc.api.clientid";
+	public static String DATA_API_CLIENTSECRETE = "htrc.api.clientsecrete";
+	public static String DATA_API_TOKENLOC = "htrc.api.tokenloc";
+	public static String DATA_API_SELFSIGNED = "htrc.api.selfsigned";
+
+	// handle URI syntax
+	public static String DATA_API_DEFAULT_URL = "dhcp-bl.indiana.edu:25443";
+	public static String DATA_API_DEFAULT_URL_PREFIX = "129-79-49-119.";
+}
